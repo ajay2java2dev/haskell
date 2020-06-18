99b2c0ccf945d0da6326b246a0afaef7d66af4b1
kamenon2 <kamenon2@illinois.edu>
2020-06-17 21:29:19 -0500

    CPS Start

--- 7_otic/cps_1.hs
+++ 7_otic/cps_1.hs
@@ -3,9 +3,15 @@ main = defaultMain
 
 --these contains examples I have tried for continous functions
 
-
+-- GCD OF A LIST
 
 -- GCD - A Continuation Solution
+-- 1. newk = aux fxn has its own continuation name = newk
+-- 2. aux now runs on 2 different continuation in its scope. One is the current continuation newk and a gcdstart newk continuation
+-- 3. base case aux calls newk with base case 0
+-- 4. the usual continuation case (\res -> newk (gcd x res)) --> aux creates a new continuation by wrapping a lambda around a current continuation.
+-- 5. the parameter res will get its results by recurrsive calls to aux. This is just a accumalator recurrsion.
+-- 6. magic happens on line 3 when we find 1 in the list. Instead of calling newk we call top continuation k instead.
 gcdstar xx k = aux xx k
     where   aux [] newk     =   newk 0
             aux (1:xs) newk =   k 1
